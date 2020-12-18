0d7ebfe5ffd1ed75217f47d8908d72840b689ed1
Christopher <chris.glood3011@gmail.com>
2020-12-18 09:41:33 -0400

    fixing token

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let color = gen().hexString();
 
 let config = {
     prefix: "js/",
-    token: "Nzg2NjM2NzQyNjQ3MDg3MTU0.X9JSjg.yXowk9MaFKbLMvCkHsrY_7PoEGc",
+    token: "Nzg2NjM2NzQyNjQ3MDg3MTU0.X9JSjg.hVcxUd0n9DNhdeCZr9mvcwvZD5w",
     ownerid: "332841209875791875",
     douid:"337804817445552130",
     embedcolor:`0x${color.slice(1)}`
