079bd1c44dbc64a66ce1b1fd3a91bbd61e973f9e
Christopher <chris.glood3011@gmail.com>
2020-12-28 09:26:01 -0400

    Configuracion local + Variables de entorno

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let color = gen().hexString();
 
 let config = {
     prefix: "js/",
-    token: "Nzg2NjM2NzQyNjQ3MDg3MTU0.X9JSjg.hVcxUd0n9DNhdeCZr9mvcwvZD5w",
+    token: process.env.TOKEN,
     ownerid: "332841209875791875",
     douid:"337804817445552130",
     embedcolor:`0x${color.slice(1)}`
